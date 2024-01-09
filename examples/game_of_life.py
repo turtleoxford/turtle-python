c060bbc48642d23f1aa5eacd5d14d3d50acc9fed
Ioana Vasile <ioanav104@gmail.com>
2024-01-09 14:45:16 +0000

    Add support for keyboard input

--- examples/game_of_life.py
+++ examples/game_of_life.py
@@ -8,19 +8,24 @@ with turtle_canvas(0, 0, 900, 900) as t:
     for x in range(width):
         for y in range(height):
             if randint(0, 6) == 0:
-                pixset(x, y, 0xfffffd)
-    while True:
+                pixset(x, y, 0xFFFFFD)
+    while get_key_sym() != "Escape":
         noupdate()
         for x in range(width):
             for y in range(height):
                 dn = 0
-                for i in range (-1, 2, 1):
+                for i in range(-1, 2, 1):
                     for j in range(-1, 2, 1):
-                        dn += pixcol((x + i + width) % width, (y + j + height) % height) & 1
-                if ((pixcol(x, y) & 1 == 0) and (dn < 5 or dn > 6)) or ((pixcol(x,y) & 1 == 1) and (dn == 6)):
-                    pixset(x, y, pixcol(x,y)^2)
+                        dn += (
+                            pixcol((x + i + width) % width, (y + j + height) % height)
+                            & 1
+                        )
+                if ((pixcol(x, y) & 1 == 0) and (dn < 5 or dn > 6)) or (
+                    (pixcol(x, y) & 1 == 1) and (dn == 6)
+                ):
+                    pixset(x, y, pixcol(x, y) ^ 2)
         for x in range(width):
             for y in range(height):
-                if (pixcol(x,y)&3)%3!=0:
-                    pixset(x,y,pixcol(x,y)^0xFFFFFD)
+                if (pixcol(x, y) & 3) % 3 != 0:
+                    pixset(x, y, pixcol(x, y) ^ 0xFFFFFD)
             update()
---
+++ examples/user_interaction.py
@@ -0,0 +1,11 @@
+from turtle_oxford import *
+
+with turtle_canvas() as t:
+    update()
+    s: str = ""
+    blank(0xFFFDD0)
+    while s != "Escape":
+        blank(0xFFFDD0)
+        display(get_key_code())
+    blank(0xFFFDD0)
+    display("Done")
